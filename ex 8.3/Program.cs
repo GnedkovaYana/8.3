c76adce8da0b36aca0348a468e17386c712e833f
Gnedkova Yana <ygnedkova@gmail.com>
2022-12-25 18:40:43 +0500

    commit2

--- ex 8.3/Program.cs
+++ ex 8.3/Program.cs
@@ -6,7 +6,7 @@ namespace Anagrams
     {
         static void Main()
         {
-            string[] array = new string[] { "code", "doce", "ecod", "framer", "frame" };
+            string[] array = new string[] { "code", "doce", "ecod", "framer", "frame", "Илья" };
             string[] changedArray = DeleteAnagramsFromArray(array);
             foreach (string s in changedArray)
                 Console.WriteLine(s);
@@ -21,12 +21,32 @@ namespace Anagrams
 
             for (int i = 1; i < array.Length; i++)
             {
-                if (!array[i].IsAnagramTo(array[i - 1]))
+                if (!IsAnagramTo(array[i], array[i - 1]))
                     list.Add(array[i]);
             }
 
             list.Sort();
             return list.ToArray();
+        }
+
+        private static bool IsAnagramTo(string word1, string word2)
+        {
+            bool result = true;
+            char[] chars1 = word1.ToCharArray();
+            char[] chars2 = word2.ToCharArray();
+            Array.Sort(chars1);
+            Array.Sort(chars2);
+            if (chars1.Length != chars2.Length)
+                return false;
+            else
+            {
+                for (int i = 0; i < chars1.Length; i++)
+                {
+                    if (chars1[i] != chars2[i])
+                        result = false;
+                }
+            }
+            return result;
         }
     }
 }
